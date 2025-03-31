b51a806da9e2bd47808212e301b420a834cb2eb1
dammyayoola72 <dammyayoola72@gmail.com>
2025-03-30 22:06:41 -0500

    first release

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "securestorage" {
+    resource_group_name = var.resource_group_name
+    name = var.storage_account_name
+    location = var.location
+    account_tier = var.account_tier
+    account_replication_type = var.account_replication_type
+  
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+  default = "GRS"
+}
+
+variable "account_tier" {
+    type = string
+    default = "standard"
+  
+}
